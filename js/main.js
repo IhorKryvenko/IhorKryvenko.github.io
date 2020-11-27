329f1c2a3b53b2ee41a73b51927fa9fd4375c143
IhorKryvenko <ihor.krivenko@gmail.com>
2020-11-27 19:36:19 +0200

    1.1.03

--- js/main.js
+++ js/main.js
@@ -10,6 +10,7 @@
         const ease = function(t,b,c,d) {
             t /= d / 2;
             if (t < 1) return c / 2 * t * t + b;
+            
             t--;
             return -c / 2 * (t * (t - 2) - 1) + b;
         };
